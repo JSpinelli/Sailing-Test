8ed2e29c37b3b23a0012d3549d5f07fb93d5dd6f
julian spinelli <julianspi95@gmail.com>
2021-11-29 14:02:53 -0500

    New Packages

--- Assets/Scripts/WaterManager.cs
+++ Assets/Scripts/WaterManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
@@ -8,14 +9,21 @@ using UnityEngine;
 public class WaterManager : MonoBehaviour
 {
     private MeshFilter _meshFilter;
+    private Material waves;
+    private static readonly int Direction = Shader.PropertyToID("_FoamDirection");
 
     private void Awake()
     {
         _meshFilter = GetComponent<MeshFilter>();
+        waves = GetComponent<MeshRenderer>().sharedMaterial;
     }
 
     private void Update()
     {
+        // Vector2 direction = Vector2.left - WindManager.instance.wind;
+        // float angle = Mathf.Atan2(direction.y,  direction.x) * Mathf.Rad2Deg;
+        // if (angle < 0f) angle += 360f;
+        // waves.SetFloat(Direction, ((angle/360)*2)-1);
         MeshUpdate();
     }
     
@@ -26,7 +34,6 @@ public class WaterManager : MonoBehaviour
         {
             vertices[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x, transform.position.z + vertices[i].z);
         }
-
         _meshFilter.mesh.vertices = vertices;
         _meshFilter.mesh.RecalculateBounds();
         _meshFilter.mesh.RecalculateNormals();
--- Assets/Scripts/WindManager.cs
+++ Assets/Scripts/WindManager.cs
@@ -1,16 +1,33 @@
-﻿using System;
-using System.ComponentModel;
+﻿using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class WindManager : MonoBehaviour
 {
     public static WindManager instance;
-
     public Vector2 wind;
     public float windMagnitude;
     
+    public float minimumTimeBeforeChange;
+    public float maximumTimeBeforeChange;
+    
+    public float maximumWindChange = 180;
+    public float minimumWindChange = 10;
+    
+    public float maximumWindMagnitude = 10;
+    public float minimumWindMagnitude = 1;
+
+    public float transitionTime = 2;
+    
+    
     public float noGo = -0.45f;
+    
+    
+    private float timeBeforeChange;
+    private float windChangeTimer;
+    private float targetMagnitude;
+    private Vector2 targetDirection;
+    private float timeToFullTransition = 0;
     private void Awake()
     {
         if (instance == null)
@@ -24,8 +41,55 @@ public class WindManager : MonoBehaviour
         }
     }
 
-    public Vector2 GetWind(float x)
+    private void Start()
+    {
+        wind = RandomizeWind();
+        timeBeforeChange = Random.Range(minimumTimeBeforeChange, maximumTimeBeforeChange);
+        windMagnitude = Random.Range(minimumWindMagnitude, maximumWindMagnitude);
+        windChangeTimer = 0;
+        timeToFullTransition = 0;
+        targetMagnitude = Random.Range(minimumWindMagnitude, maximumWindMagnitude);
+        RotateWind();
+    }
+
+    private void Update()
+    {
+        if (windChangeTimer < timeBeforeChange)
+        {
+            windChangeTimer += Time.deltaTime;
+        }
+        else
+        {
+            if (timeToFullTransition < transitionTime)
+            {
+                windMagnitude = Mathf.Lerp(windMagnitude, targetMagnitude, Time.deltaTime);
+                wind = Vector2.Lerp(wind, targetDirection, Time.deltaTime);
+                timeToFullTransition += Time.deltaTime;
+            }
+            else
+            {
+                windChangeTimer = 0;
+                timeToFullTransition = 0;
+                timeBeforeChange = Random.Range(minimumTimeBeforeChange, maximumTimeBeforeChange);
+                targetMagnitude = Random.Range(minimumWindMagnitude, maximumWindMagnitude);
+                RotateWind();
+            }
+        }
+    }
+
+    public Vector2 GetWind()
     {
         return wind.normalized;
+    }
+
+    public Vector2 RandomizeWind()
+    {
+        return new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
+        
+    }
+
+    public void RotateWind()
+    {
+        targetDirection = Quaternion.Euler(0, 0, Random.Range(minimumWindChange, maximumWindChange)) * wind;
     }
 }
